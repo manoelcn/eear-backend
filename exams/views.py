d66e7cc72b9dccf251c12821def62202f3a1e253
manoel <candidomanoel78@gmail.com>
2024-11-28 13:40:19 -0300

    apagando linhas desnecessarias

--- exams/views.py
+++ exams/views.py
@@ -1,14 +1,8 @@
 from .models import Exam
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 
 
 class ExamListView(ListView):
     model = Exam
     template_name = 'exam_list.html'
-    context_object_name = 'exam'
-
-
-class ExamDetailView(DetailView):
-    model = Exam
-    template_name = 'exam_detail.html'
     context_object_name = 'exam'
