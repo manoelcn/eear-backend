1da927006db5033b882386f8c5219c41f2786a17
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-28 10:38:06 -0300

    criando models de news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    date_publication = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='news/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
