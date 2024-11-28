af6a49fc99786381fb7bc013d27a03c4d305bad3
manoel <candidomanoel78@gmail.com>
2024-11-28 13:36:51 -0300

    criando models

--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    title = models.CharField(max_length=100)
+    date_publication = models.DateTimeField(auto_now_add=True)    
+    description = models.CharField(max_length=500)
+    file = models.FileField(upload_to='exam/')
+
+    def __str__(self):
+        return self.title
