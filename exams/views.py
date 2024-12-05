d33cc45f1b04f4f68a92badbd0b5cce15a28f875
manoel <candidomanoel78@gmail.com>
2024-12-05 17:33:05 -0300

    add filtro de mais recente e mais antigo na pagina de provas

--- exams/views.py
+++ exams/views.py
@@ -7,3 +7,19 @@ class ExamListView(ListView):
     template_name = 'exam_list.html'
     context_object_name = 'exam'
     paginate_by = 3
+
+    
+    def get_queryset(self):
+        queryset = Exam.objects.all()
+
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query)
+
+        order_by = self.request.GET.get('order_by', 'recent')
+        if order_by == 'old':
+            queryset = queryset.order_by('date_publication')
+        else:
+            queryset = queryset.order_by('-date_publication')
+
+        return queryset
