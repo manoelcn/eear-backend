2d7af584a60736c3acfc8db3d90145f01b93eaa2
Manoel Cândido <candidomanoel78@gmail.com>
2024-12-05 11:25:36 -0300

    formatando view

--- app/views.py
+++ app/views.py
@@ -7,15 +7,14 @@ def home(request):
     exams = Exam.objects.order_by('-date_publication')[:2]
     news = News.objects.order_by('-date_publication')[:2]
 
-    # Adicionando paginação para news_card
     news_card_list = News.objects.all()
-    paginator = Paginator(news_card_list, 3)  # 3 itens por página
-    page_number = request.GET.get('page')  # Captura o número da página atual
-    news_card = paginator.get_page(page_number)  # Obtém a página atual
+    paginator = Paginator(news_card_list, 3)
+    page_number = request.GET.get('page')
+    news_card = paginator.get_page(page_number)
 
     context = {
         'exams': exams,
         'news': news,
-        'news_card': news_card,  # Passa apenas a página atual
+        'news_card': news_card,
     }
     return render(request, 'home.html', context)
