1825d204b76b82a6fc87ca45fb528f0cf2ec606c
Manoel Cândido <candidomanoel78@gmail.com>
2024-12-05 11:01:01 -0300

    add news_card

--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from news.models import News
 def home(request):
     exams = Exam.objects.order_by('-date_publication')[:2]
     news = News.objects.order_by('-date_publication')[:2]
+    news_card = News.objects.all()
 
     context = {
         'exams': exams,
-        'news': news
+        'news': news,
+        'news_card': news_card
     }
     return render(request, 'home.html', context)
