fffee851681ce2dc8f0a81e4ae93b736c7b03d38
manoel <candidomanoel78@gmail.com>
2024-12-05 17:44:39 -0300

    add filtro de mais recentes e antigos na pagina de noticias

--- news/views.py
+++ news/views.py
@@ -9,6 +9,22 @@ class NewsListView(ListView):
     paginate_by = 3
 
 
+    def get_queryset(self):
+        queryset = News.objects.all()
+
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            queryset = queryset.filter(title_icontains=search_query)
+        
+        order_by = self.request.GET.get('order_by', 'recent')
+        if order_by == 'old':
+            queryset = queryset.order_by('date_publication')
+        else:
+            queryset = queryset.order_by('-date_publication')
+
+        return queryset
+
+
 class NewsDetailView(DetailView):
     model = News
     template_name = 'news_detail.html'
