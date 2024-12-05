874f319e1e67d1324e562cc9e768810bca6004f3
manoel <candidomanoel78@gmail.com>
2024-12-05 17:21:03 -0300

    add filtro de mais recentes e mais antigos

--- app/views.py
+++ app/views.py
@@ -4,17 +4,24 @@ from exams.models import Exam
 from news.models import News
 
 def home(request):
-    exams = Exam.objects.order_by('-date_publication')[:2]
-    news = News.objects.order_by('-date_publication')[:2]
+    filter_option = request.GET.get('filter', 'recent')
 
-    news_card_list = News.objects.all()
+    if filter_option == 'old':
+        news_card_list = News.objects.all().order_by('date_publication')
+    else:
+        news_card_list = News.objects.all().order_by('-date_publication')
+
     paginator = Paginator(news_card_list, 3)
     page_number = request.GET.get('page')
     news_card = paginator.get_page(page_number)
+
+    exams = Exam.objects.order_by('-date_publication')[:2]
+    news = News.objects.order_by('-date_publication')[:2]
 
     context = {
         'exams': exams,
         'news': news,
         'news_card': news_card,
+        'filter_option': filter_option,
     }
     return render(request, 'home.html', context)
